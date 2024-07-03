fbd7be3ab2067027e8e50b6d72f39247cdb358ff
Dushyant Rajeev Bhardwaj <dushyantbhardwaj26@gmail.com>
2024-07-03 15:06:43 +0530

    connect to MongoDB

--- api/index.js
+++ api/index.js
@@ -1,5 +1,10 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
+dotenv.config();
+
+mongoose.connect(process.env.MONGO)
 const app = express();
 
 app.listen(3000, ()=>{
