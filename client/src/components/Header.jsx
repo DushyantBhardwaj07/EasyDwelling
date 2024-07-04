d89820e1a0d555e4729bdae74c91b1d965dfd954
Dushyant Rajeev Bhardwaj <dushyantbhardwaj26@gmail.com>
2024-07-04 20:52:58 +0530

    update the header and make the profile page private

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,53 +1,75 @@
-import React, { useState } from 'react'
-import { FaSearch, FaBars } from 'react-icons/fa'
-import { Link } from 'react-router-dom'
+import { FaSearch } from 'react-icons/fa';
+import { Link, useNavigate } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { useEffect, useState } from 'react';
 
 export default function Header() {
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const { currentUser } = useSelector((state) => state.user);
+  const [searchTerm, setSearchTerm] = useState('');
+  const navigate = useNavigate();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const urlParams = new URLSearchParams(window.location.search);
+    urlParams.set('searchTerm', searchTerm);
+    const searchQuery = urlParams.toString();
+    navigate(`/search?${searchQuery}`);
+  };
 
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const searchTermFromUrl = urlParams.get('searchTerm');
+    if (searchTermFromUrl) {
+      setSearchTerm(searchTermFromUrl);
+    }
+  }, [location.search]);
   return (
     <header className='bg-slate-200 shadow-md'>
       <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
         <Link to='/'>
-          <h1 className='font-bold text-xl sm:text-2xl flex flex-wrap'>
-            <span className='text-slate-500'>Easy</span>
-            <span className='text-slate-700'>Dwelling</span>
-          </h1>            
+          <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
+            <span className='text-slate-500'>Sahand</span>
+            <span className='text-slate-700'>Estate</span>
+          </h1>
         </Link>
-
-        <form className='bg-slate-100 p-3 rounded-lg flex items-center'>
-          <input 
-            type="text" 
-            placeholder="Search..." 
+        <form
+          onSubmit={handleSubmit}
+          className='bg-slate-100 p-3 rounded-lg flex items-center'
+        >
+          <input
+            type='text'
+            placeholder='Search...'
             className='bg-transparent focus:outline-none w-24 sm:w-64'
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
           />
-          <FaSearch className='text-slate-600' />
-        </form>
-
-        <nav className="hidden md:block">
-          <ul className="flex space-x-4">
-            <li><Link to="/" className='text-slate-700 font-bold hover:underline text-lg'>Home</Link></li>
-            <li><Link to="/about" className='text-slate-700 font-bold hover:underline text-lg'>About</Link></li>
-            <li><Link to="/sign-in" className='text-slate-700 font-bold hover:underline text-lg'>Sign In</Link></li>
-          </ul>
-        </nav>
-
-        <div className="md:hidden">
-          <button onClick={() => setIsMenuOpen(!isMenuOpen)}>
-            <FaBars className="text-slate-700 text-2xl" />
+          <button>
+            <FaSearch className='text-slate-600' />
           </button>
-        </div>
+        </form>
+        <ul className='flex gap-4'>
+          <Link to='/'>
+            <li className='hidden sm:inline text-slate-700 hover:underline'>
+              Home
+            </li>
+          </Link>
+          <Link to='/about'>
+            <li className='hidden sm:inline text-slate-700 hover:underline'>
+              About
+            </li>
+          </Link>
+          <Link to='/profile'>
+            {currentUser ? (
+              <img
+                className='rounded-full h-7 w-7 object-cover'
+                src={currentUser.avatar}
+                alt='profile'
+              />
+            ) : (
+              <li className=' text-slate-700 hover:underline'> Sign in</li>
+            )}
+          </Link>
+        </ul>
       </div>
-
-      {isMenuOpen && (
-        <nav className="md:hidden">
-          <ul className="flex flex-col items-center py-4">
-            <li className='py-2'><Link to="/" className='text-slate-700 font-bold hover:underline text-lg'>Home</Link></li>
-            <li className='py-2'><Link to="/about" className='text-slate-700 font-bold hover:underline text-lg'>About</Link></li>
-            <li className='py-2'><Link to="/sign-in" className='text-slate-700 font-bold hover:underline text-lg'>Sign In</Link></li>
-          </ul>
-        </nav>
-      )}
     </header>
-  )
+  );
 }
