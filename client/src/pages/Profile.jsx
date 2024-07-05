a82b4d750b4b1b26b40fe997c0b55e215ecead47
Dushyant Rajeev Bhardwaj <dushyantbhardwaj26@gmail.com>
2024-07-06 00:00:23 +0530

    Completed Delete User Listing Functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -141,6 +141,25 @@ export default function Profile() {
       setShowListingsError(true);
     }
   };
+  
+  const handleListingDelete = async (listingId) => {
+    try {
+      const res = await fetch(`/api/listing/delete/${listingId}`, {
+        method: 'DELETE',
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        console.log(data.message);
+        return;
+      }
+
+      setUserListings((prev) =>
+        prev.filter((listing) => listing._id !== listingId)
+      );
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
 
   return (
     <div className='p-3 max-w-lg mx-auto'>
@@ -246,6 +265,7 @@ export default function Profile() {
         userListings.length > 0 &&
         <div className="flex flex-col gap-4">
           <h1 className='text-center mt-7 text-2xl font-semibold'>Your Listings</h1>
+          
           {userListings.map((listing) => (
             <div
               key={listing._id}
@@ -266,7 +286,14 @@ export default function Profile() {
               </Link>
 
               <div className='flex flex-col item-center'>
-                <button className='text-red-700 uppercase'>Delete</button>
+
+              <button
+                  onClick={() => handleListingDelete(listing._id)}
+                  className='text-red-700 uppercase'
+                >
+                  Delete
+                </button>
+
                 <button className='text-green-700 uppercase'>Edit</button>
               </div>
             </div>
